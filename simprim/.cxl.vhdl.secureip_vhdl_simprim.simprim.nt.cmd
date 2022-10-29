69bdda74d698da6c211760fde8b72fe5989bfbda
Alfred Zhang <52687013+alfredzhang98@users.noreply.github.com>
2022-10-29 17:29:07 +0100

    updata data

---
+++ simprim/.cxl.vhdl.secureip_vhdl_simprim.simprim.nt.cmd
@@ -0,0 +1,3 @@
+C:\modeltech_10.1c\win32/vlib C:\Xilinx\14.1\ISE_DS\ISE\vhdl\mti_se\10.1c\nt/simprim
+C:\modeltech_10.1c\win32/vmap simprim C:\Xilinx\14.1\ISE_DS\ISE\vhdl\mti_se\10.1c\nt/simprim
+C:\modeltech_10.1c\win32/vcom -source -93 -novopt -explicit -32 -work simprim -f C:\Xilinx\14.1\ISE_DS\ISE\vhdl\mti_se\10.1c\nt/simprim/.cxl.vhdl.secureip_vhdl_simprim.simprim.nt.cmf
